d0c3089c6f7947f5882c607c36a262e43c7c587b
Alexander Biryukov <alex.biryukoff@gmail.com>
2016-10-30 19:32:26 +0700

    Add simple stub rendering

--- src/App.elm
+++ src/App.elm
@@ -1,19 +1,72 @@
 module App exposing (..)
 
-import Html exposing (text, div)
+import Html exposing (Html, div, input, button, text, ul, li, p, label)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick, onInput)
+import Regex exposing (regex, contains)
 
 
+-- MODEL
+
+type alias Model =
+     List Location
+
+type alias Weather =
+     String
+
+type alias Location =
+    { zip : String
+    , temp : Maybe Weather
+    }
+
+-- SUBSCRIPTIONS
+
+subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.none
 
+-- UPDATE
 
+type Msg
+    = AddLocation
+    | ZipChange String
+
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        AddLocation ->
+            ( [], Cmd.none )
 
+        ZipChange zip ->
+            if contains (regex "^\\d{5}$") zip then
+                ( { zip = zip, temp = Nothing } :: model, Cmd.none)
+            else
+                ( model, Cmd.none )
 
+-- INIT
+
+init : (Model, Cmd Msg)
 init =
-    ( (), Cmd.none )
+    ( [], Cmd.none )
 
+-- VIEW
 
+view : Model -> Html Msg
 view model =
-    div [] [ text "Your Elm App is working!" ]
+    div [ id "app-container" ]
+        ([ div [ id "form" ]
+            [ label [] [ text "Zip Code:"]
+            , input [ placeholder "Enter Zip",  onInput ZipChange] []
+            , button [ onClick AddLocation ] [ text "Add location" ]
+            ]
+        ] ++ (List.map viewLocation model))
+
+viewLocation : Location -> Html msg
+viewLocation location =
+    let
+        locationTemp = Maybe.withDefault "0" location.temp
+    in
+        div [ class "location", id ("zip-" ++ location.zip) ]
+            [ p [ class "zip" ] [ text location.zip ]
+            , p [ class "temp" ] [ text locationTemp, text "℃"]
+            ]
