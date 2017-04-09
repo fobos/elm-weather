2169cc9e04d24cb500197585a205833a9dbee16f
Alexander Biryukov <alex.biryukoff@gmail.com>
2017-04-10 01:01:02 +0700

    Prepare for writing tests

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -3,18 +3,13 @@ module Tests exposing (..)
 import Test exposing (..)
 import Expect
 import String
+import App
 
 
 all : Test
 all =
     describe "A Test Suite"
-        [ test "Addition" <|
+        [ test "Pass" <|
             \() ->
-                Expect.equal (3 + 7) 10
-        , test "String.left" <|
-            \() ->
-                Expect.equal "a" (String.left 1 "abcdefg")
-        , test "This test should fail" <|
-            \() ->
-                Expect.fail "failed as expected!"
+                Expect.pass
         ]
